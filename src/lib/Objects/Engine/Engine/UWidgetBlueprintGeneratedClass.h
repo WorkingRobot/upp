6b51d8970590f36bdb856d9c93196152e8de941e
Aleks <aleks.margarian1@gmail.com>
2021-08-05 10:11:46 -0700

    whoops

--- src/lib/Objects/Engine/Engine/UWidgetBlueprintGeneratedClass.h
+++ src/lib/Objects/Engine/Engine/UWidgetBlueprintGeneratedClass.h
@@ -3,7 +3,7 @@
 #include "UBlueprintGeneratedClass.h"
 
 namespace upp::Objects {
-    struct UWidgetBlueprintGeneratedClass : public UClass {
+    struct UWidgetBlueprintGeneratedClass : public UBlueprintGeneratedClass {
         // https://github.com/EpicGames/UnrealEngine/blob/c3caf7b6bf12ae4c8e09b606f10a09776b4d1f38/Engine/Source/Runtime/UMG/Private/WidgetBlueprintGeneratedClass.cpp#L400
         UWidgetBlueprintGeneratedClass(FArchive& Ar, const Providers::Schema& Schema, FSerializeCtx& Ctx, bool IsCDO) : UBlueprintGeneratedClass(Ar, Schema, Ctx, IsCDO) { }
     };
